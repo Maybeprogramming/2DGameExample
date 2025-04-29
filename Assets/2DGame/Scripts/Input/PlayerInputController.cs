1b5cfc8e7af07f6519e4f3b147b25616cb89f86d
Maybeonce-HomePc <a-u-rom@yandex.ru>
2025-04-29 23:53:28 +0400

    +

--- Assets/2DGame/Scripts/Input/PlayerInputController.cs
+++ Assets/2DGame/Scripts/Input/PlayerInputController.cs
@@ -3,45 +3,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class PlayerInputController : MonoBehaviour
 {
-    private InputControls _inputs;
+    private InputModule _inputs;
 
     private void Awake()
     {
-        _inputs = new InputControls();
+        _inputs = new InputModule();
+        Debug.Log(_inputs == null);
+        Debug.Log(_inputs.Player.Attack);
     }
 
     private void Start()
-    {     
-        _inputs.Player.Walk.performed += OnWalking;
-        _inputs.Player.Attack.performed += Attack;
+    {
     }
 
-    private void Attack(InputAction.CallbackContext context)
+    private void OnAttack(InputAction.CallbackContext context)
     {
-        Debug.Log($"{context}");
+        Debug.Log($"Атака");
     }
 
     private void OnEnable()
     {
+        _inputs.Enable();
+        _inputs.Player.Walk.performed += OnWalking;
+        _inputs.Player.Attack.performed += OnAttack;
+        _inputs.Player.Jump.performed +=  OnJump;
+        _inputs.Player.HeavyAttack.performed += OnHeavyAttack;
+        _inputs.Player.Run.performed += OnRunnig;
+    }
+
+    private void OnRunnig(InputAction.CallbackContext context)
+    {
+        Debug.Log($"Бег {context.ReadValue<float>()}");
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        Debug.Log($"Прыжок");
+    }
+
+    private void OnHeavyAttack(InputAction.CallbackContext context)
+    {
+        Debug.Log($"Мега Атака");
     }
 
     private void OnDisable()
     {
         _inputs.Player.Walk.performed -= OnWalking;
-        _inputs.Player.Attack.performed -= Attack;
+        _inputs.Player.Attack.performed -= OnAttack;
+        _inputs.Disable();
 
     }
 
-    private void OnWalking(InputAction.CallbackContext callbackContext)
+    private void OnWalking(InputAction.CallbackContext context)
     {
-        Debug.Log(callbackContext.ReadValue<float>());
-    }
-
-    private void Update()
-    {
-
+        Debug.Log("Ходьба");
     }
 }
