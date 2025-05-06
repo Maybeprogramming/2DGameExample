f09c82abedaafaf62c7df8b1cbd92162a42e165e
Maybeonce <a-u-rom@yandex.ru>
2025-05-06 16:18:44 +0400

    +

--- Assets/Vamperism.cs
+++ Assets/Vamperism.cs
@@ -1,14 +1,19 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Vamperism : MonoBehaviour
 {
     [SerializeField] private PlayerInputController _input;
     [SerializeField] private float _damage;
-    [SerializeField] private float _durationActive;
-    [SerializeField] private float _durationRecharge;
+    [SerializeField] private float _durationActiveTime;
+    [SerializeField] private float _durationRechargeTime;
     [SerializeField] private float _radiusAction;
     [SerializeField] private Transform _centrPosition;
     [SerializeField] private SpriteRenderer _spriteRenderer;
+
+    public Action<float> Activated;
+    public Action Ended;
 
     private void Start()
     {
@@ -34,6 +39,13 @@ public class Vamperism : MonoBehaviour
     private void OnVamperismActevated()
     {
         Debug.Log("Вампиризм ^^");  
+        StartCoroutine(VamperismActivating(_durationActiveTime));
+    }
+
+    private IEnumerator VamperismActivating(float duration)
+    {
+        _spriteRenderer.enabled = !_spriteRenderer.enabled;
+        yield return new WaitForSeconds(duration);
         _spriteRenderer.enabled = !_spriteRenderer.enabled;
     }
 }
